e97766a4d5cd395208feeddd1f36d9637bd2147e
TTioTTi <be99999@likelion.org>
2021-12-09 13:31:45 +0900

    hello_world view update

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+# API Guide - Views - Function Based Views
+@api_view()
+def hello_world(request):
+    return Response({"message": "hello world"})
