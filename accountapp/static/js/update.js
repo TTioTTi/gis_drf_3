490e7ab4d6473f643c12ac99ef894b37ba801cfe
TTioTTi <be99999@likelion.org>
2021-12-14 15:15:28 +0900

    update API error code update

--- accountapp/static/js/update.js
+++ accountapp/static/js/update.js
@@ -22,19 +22,30 @@ function update_account(pk) {
         username: document.getElementById('username').value,
         email: document.getElementById('email').value,
     } )
+
         .then(function (response) {
             // handle success
             console.log(response);
 
             window.location.href = '/accounts/retrieve_template/' + pk;
         })
+
         .catch(function (error) {
             // handle error
             console.log(error);
 
-            document.getElementById('alert_box').innerHTML
-                = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+            if (error.response.status == 401) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>인증정보가 없습니다.</div>"
+            } else if (error.response.status == 403) {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>권한이 없습니다.</div>"
+            } else {
+                document.getElementById('alert_box').innerHTML
+                    = "<div class='btn btn-danger rounded-pill px-5'>업데이트에 실패했습니다.</div>"
+            }
         })
+
         .then(function () {
             // always executed
         });
