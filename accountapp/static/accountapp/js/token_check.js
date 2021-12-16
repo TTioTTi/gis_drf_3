841fb4a6ff1cb967173c9631c5d28bd9a49b9b8b
TTioTTi <be99999@likelion.org>
2021-12-16 14:37:41 +0900

    header button error fixed

--- accountapp/static/accountapp/js/token_check.js
+++ accountapp/static/accountapp/js/token_check.js
@@ -7,33 +7,30 @@ function getCookie(name) {
 }
 
 
-var user_id;
-
-axios({
-    method: 'get',
-    url: '/accounts/token/',
-    headers: {
-        Authorization: decodeURIComponent(getCookie('drf_token'))
-    }
-})
-.then(function (response) {
-    user_id = response.data['id'];
-    document.getElementById('signup_button').innerHTML =
-    "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
-    " MyPage\n" +
-    " </a>";
-})
-
-
 var check = getCookie('drf_token');
 
 if (check !== undefined) {
-//    document.getElementById('signup_button').innerHTML =
-//        "<a href=\"/accounts/retrieve_template/\">\n" +
-//        "MyPage\n" +
-//        "</a>";
     document.getElementById('login_button').innerHTML =
         "<a href=\"/accounts/logout_template/\">\n" +
         "Logout\n" +
         "</a>";
+
+
+    var user_id;
+
+    axios({
+        method: 'get',
+        url: '/accounts/token/',
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
+
+    .then(function (response) {
+        user_id = response.data['id'];
+        document.getElementById('signup_button').innerHTML =
+        "<a href=\"/accounts/retrieve_template/" + user_id + "\">\n" +
+        "MyPage\n" +
+        "</a>";
+    })
 }
