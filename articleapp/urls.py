1a3c2b9b23f7a9e42087b31116ac81669b963bdd
TTioTTi <be99999@likelion.org>
2021-12-17 15:47:37 +0900

    article update view update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from articleapp.views import ArticleCreateAPIView, ArticleCreateTemplateView, ArticleRUDAPIView, \
-    ArticleRetrieveTemplateView
+    ArticleRetrieveTemplateView, ArticleUpdateTemplateView, ArticleDestroyTemplateView
 
 app_name = 'articleapp'
 
@@ -9,5 +9,7 @@ urlpatterns = [
     path('create_template/', ArticleCreateTemplateView.as_view(), name='create_template'),
     path('', ArticleCreateAPIView.as_view(), name='create'),
     path('retrieve_template/<int:pk>', ArticleRetrieveTemplateView.as_view(), name='retrieve_template'),
+    path('update_template/<int:pk>', ArticleUpdateTemplateView.as_view(), name='update_template'),
+    path('destroy_template/<int:pk>', ArticleDestroyTemplateView.as_view(), name='destroy_template'),
     path('<int:pk>', ArticleRUDAPIView.as_view(), name='RUD'),
 ]
