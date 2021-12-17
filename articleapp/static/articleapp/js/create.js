68fed389c696b35769e3b01b8ff4bffc458519a0
TTioTTi <be99999@likelion.org>
2021-12-17 14:39:02 +0900

    article retrieve view / template update

--- articleapp/static/articleapp/js/create.js
+++ articleapp/static/articleapp/js/create.js
@@ -18,10 +18,7 @@ function send_input() {
         .then(function (response) {
             console.log(response);
 
-            // window.location.href = '/articles/retrieve_template/' + response.data['id'];
-
-            document.getElementById('alert_box').innerHTML
-                = "<div class='btn btn-primary rounded-pill px-5'>게시글 작성에 성공했습니다</div>"
+            window.location.href = '/articles/retrieve_template/' + response.data['id'];
         })
 
         .catch(function (error) {
--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateAPIView, ArticleCreateTemplateView, ArticleRUDAPIView
+from articleapp.views import ArticleCreateAPIView, ArticleCreateTemplateView, ArticleRUDAPIView, \
+    ArticleRetrieveTemplateView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create_template/', ArticleCreateTemplateView.as_view(), name='create_template'),
     path('', ArticleCreateAPIView.as_view(), name='create'),
+    path('retrieve_template/<int:pk>', ArticleRetrieveTemplateView.as_view(), name='retrieve_template'),
     path('<int:pk>', ArticleRUDAPIView.as_view(), name='RUD'),
 ]
