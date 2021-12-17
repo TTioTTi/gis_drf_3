441ffae6208d2f18b78506b77b0f2695592056b3
TTioTTi <be99999@likelion.org>
2021-12-17 10:36:43 +0900

    article create template view update

---
+++ articleapp/static/articleapp/js/create.js
@@ -0,0 +1,33 @@
+
+function send_input() {
+
+    var form = new FormData();
+    form.append('title', document.getElementById('title').value);
+    form.append('content', document.getElementById('content').value);
+    form.append('image', document.getElementById('image').files[0]);
+
+    axios({
+        method: 'post',
+        url: '/articles/',
+        data: form,
+        headers: {
+            Authorization: decodeURIComponent(getCookie('drf_token'))
+        }
+    })
+
+        .then(function (response) {
+            console.log(response);
+
+            // window.location.href = '/articles/retrieve_template/' + response.data['id'];
+
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-primary rounded-pill px-5'>게시글 작성에 성공했습니다</div>"
+        })
+
+        .catch(function (error) {
+            console.log(error);
+
+            document.getElementById('alert_box').innerHTML
+                = "<div class='btn btn-danger rounded-pill px-5'>게시글 작성에 실패했습니다</div>"
+        });
+}
